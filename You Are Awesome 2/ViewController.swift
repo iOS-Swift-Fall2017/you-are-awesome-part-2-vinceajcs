817ed5563c81232508bc29d3d0912706eb2e15e3
Vincent Stephen Huang <hvincentstephen@Vincents-MacBook-Pro.local>
2017-09-11 14:07:36 -0400

    Initial Commit

---
+++ You Are Awesome 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome 2
+//
+//  Created by Vincent Stephen Huang on 9/11/17.
+//  Copyright © 2017 Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
